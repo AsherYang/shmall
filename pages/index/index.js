6940955858dbb4aa71fedb2711e69a579baf6986
ouyangfan <ouyangfan1991@gmail.com>
2017-01-02 23:29:33 +0800

    update index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,43 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+   hideSwiper: false,
+   imgUrls: [ 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+  indicatorDots: true,
+  autoplay: false,
+  interval: 5000,
+  duration: 1000,
+
+ navList: [
+    {id:1, name:'辣条'},
+    {id:2, name:"方便面"},
+    {id:3, name:"啤酒"},
+    {id:4, name:"巧克力"},
+    {id:5, name:"香烟"}
+  ],
+
+  curNav:1,
+  curIndex:0,
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
+
+  selectNav(event) {
+      let id = event.target.dataset.id,
+          index = parseInt(event.target.dataset.index);
+          self = this;
+      this.setData({
+          curNav:id,
+          curIndex:index
       })
-    })
-  }
+  },
+
+
+  handletouchmove(event) {
+    console.log( + " ----- clientY = " + event.touches[0].clientY);
+  
+  },
+
 })
