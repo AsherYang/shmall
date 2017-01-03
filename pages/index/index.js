2d1b6201f52b3a36f8eb16134bbacf6d1eb82478
ouyangfan <ouyangfan1991@gmail.com>
2017-01-04 00:56:24 +0800

    shmall index goods page

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,43 +3,77 @@
 var app = getApp()
 Page({
   data: {
-   hideSwiper: false,
-   imgUrls: [ 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+    windowHeight: 0,
+    windowWidth: 0,
+
+    imgUrls: ['http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-  indicatorDots: true,
-  autoplay: false,
-  interval: 5000,
-  duration: 1000,
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
 
- navList: [
-    {id:1, name:'辣条'},
-    {id:2, name:"方便面"},
-    {id:3, name:"啤酒"},
-    {id:4, name:"巧克力"},
-    {id:5, name:"香烟"}
-  ],
+    navList: [
+      { id: 1, name: '辣条' },
+      { id: 2, name: "方便面" },
+      { id: 3, name: "啤酒" },
+      { id: 4, name: "巧克力" },
+      { id: 5, name: "香烟" },
+      { id: 1, name: '辣条' },
+      { id: 2, name: "方便面" },
+      { id: 3, name: "啤酒" },
+      { id: 4, name: "巧克力" },
+      { id: 5, name: "香烟" },
+      { id: 1, name: '辣条' },
+      { id: 2, name: "方便面" },
+      { id: 3, name: "啤酒" },
+      { id: 4, name: "巧克力" },
+      { id: 5, name: "香烟" },
+    ],
 
-  curNav:1,
-  curIndex:0,
+    curNav: 1,
+    curIndex: 0,
 
+    touchStart: 0,
   },
 
+  // 界面显示后，计算实际屏幕的宽高
+  onShow: function (e) {
+    self = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        // success
+        self.setData({
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth,
+        })
+      }
+    })
+  },
+
+  // 选择导航条目
   selectNav(event) {
-      let id = event.target.dataset.id,
-          index = parseInt(event.target.dataset.index);
-          self = this;
-      this.setData({
-          curNav:id,
-          curIndex:index
-      })
+    let id = event.target.dataset.id,
+      index = parseInt(event.target.dataset.index);
+    console.log("id = " + event.target.dataset.id + " , index = " + event.target.dataset.index);
+    self = this;
+    this.setData({
+      curNav: id,
+      curIndex: index
+    });
   },
 
+  // 上拉加载更多
+  onLoadMore: function() {
+    console.log("---- onLoadMore----");
+    // 显示导航条加载动画
+    wx.showNavigationBarLoading();
+    // 隐藏导航条加载动画
+    // wx.hideNavigationBarLoading();
+  }
 
-  handletouchmove(event) {
-    console.log( + " ----- clientY = " + event.touches[0].clientY);
-  
-  },
+
 
 })
